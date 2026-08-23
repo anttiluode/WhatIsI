78ce18922fd310a119d454337b789d550934e8e0
anttiluode <67085641+anttiluode@users.noreply.github.com>
2026-08-23 15:34:03 +0300

    Add package init

---
+++ whatisi/__init__.py
@@ -0,0 +1,5 @@
+"""WhatIsI: computational experiments on persistent deictic self-state."""
+
+from .gate0 import Gate0Config, CausalSelfPointer, run_lifetime, run_gate0
+
+__all__ = ["Gate0Config", "CausalSelfPointer", "run_lifetime", "run_gate0"]
